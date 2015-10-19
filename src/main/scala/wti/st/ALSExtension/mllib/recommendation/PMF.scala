6e8448ee8f4fc98c986082c5e7ce5ae552dd982a
LiuShifeng <LiuShifeng@users.noreply.github.com>
2015-10-19 14:09:23 +0800

    Update PMF.scala
    This version is based on spark 1.3.1

--- src/main/scala/wti/st/ALSExtension/mllib/recommendation/PMF.scala
+++ src/main/scala/wti/st/ALSExtension/mllib/recommendation/PMF.scala
@@ -18,24 +18,27 @@
  * limitations under the License.
  */
 
-package wti.st.ALSExtension.mllib.recommendation
+package org.apache.spark.mllib.recommendation
 
 import org.apache.spark.Logging
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.api.java.JavaRDD
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
-import wti.st.ALSExtension.ml.recommendation.{PMF => NewPMF}
+import org.apache.spark.ml.recommendation.{PMF => NewPMF}
 
-/**
- * A more compact class to represent a rating than Tuple3[Int, Int, Double].
- */
-@Since("0.8.0")
-case class Rating @Since("0.8.0") (
-                                    @Since("0.8.0") user: Int,
-                                    @Since("0.8.0") product: Int,
-                                    @Since("0.8.0") rating: Double)
+///**
+// * A more compact class to represent a rating than Tuple3[Int, Int, Double].
+// */
+//case class Rating(user: Int, product: Int, rating: Double)
+///**
+// * A more compact class to represent a rating than Tuple3[Int, Int, Double].
+// */
+//@Since("0.8.0")
+//case class Rating @Since("0.8.0") (
+//                                    @Since("0.8.0") user: Int,
+//                                    @Since("0.8.0") product: Int,
+//                                    @Since("0.8.0") rating: Double)
 
 /**
  * Alternating Least Squares matrix factorization.
@@ -66,7 +69,7 @@ case class Rating @Since("0.8.0") (
  * indicated user
  * preferences rather than explicit ratings given to items.
  */
-@Since("0.8.0")
+//@Since("0.8.0")
 class PMF private (
                     private var numUserBlocks: Int,
                     private var numProductBlocks: Int,
@@ -82,7 +85,7 @@ class PMF private (
    * Constructs an ALS instance with default parameters: {numBlocks: -1, rank: 10, iterations: 10,
    * lambda: 0.01, implicitPrefs: false, alpha: 1.0}.
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def this() = this(-1, -1, 10, 10, 0.01, false, 1.0)
 
   /** If true, do alternating nonnegative least squares. */
@@ -99,7 +102,7 @@ class PMF private (
    * Set the number of blocks for both user blocks and product blocks to parallelize the computation
    * into; pass -1 for an auto-configured number of blocks. Default: -1.
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def setBlocks(numBlocks: Int): this.type = {
     this.numUserBlocks = numBlocks
     this.numProductBlocks = numBlocks
@@ -109,7 +112,7 @@ class PMF private (
   /**
    * Set the number of user blocks to parallelize the computation.
    */
-  @Since("1.1.0")
+//  @Since("1.1.0")
   def setUserBlocks(numUserBlocks: Int): this.type = {
     this.numUserBlocks = numUserBlocks
     this
@@ -118,35 +121,35 @@ class PMF private (
   /**
    * Set the number of product blocks to parallelize the computation.
    */
-  @Since("1.1.0")
+//  @Since("1.1.0")
   def setProductBlocks(numProductBlocks: Int): this.type = {
     this.numProductBlocks = numProductBlocks
     this
   }
 
   /** Set the rank of the feature matrices computed (number of features). Default: 10. */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def setRank(rank: Int): this.type = {
     this.rank = rank
     this
   }
 
   /** Set the number of iterations to run. Default: 10. */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def setIterations(iterations: Int): this.type = {
     this.iterations = iterations
     this
   }
 
   /** Set the regularization parameter, lambda. Default: 0.01. */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def setLambda(lambda: Double): this.type = {
     this.lambda = lambda
     this
   }
 
   /** Sets whether to use implicit preference. Default: false. */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def setImplicitPrefs(implicitPrefs: Boolean): this.type = {
     this.implicitPrefs = implicitPrefs
     this
@@ -155,14 +158,14 @@ class PMF private (
   /**
    * Sets the constant used in computing confidence in implicit ALS. Default: 1.0.
    */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def setAlpha(alpha: Double): this.type = {
     this.alpha = alpha
     this
   }
 
   /** Sets a random seed to have deterministic results. */
-  @Since("1.0.0")
+//  @Since("1.0.0")
   def setSeed(seed: Long): this.type = {
     this.seed = seed
     this
@@ -172,7 +175,7 @@ class PMF private (
    * Set whether the least-squares problems solved at each iteration should have
    * nonnegativity constraints.
    */
-  @Since("1.1.0")
+//  @Since("1.1.0")
   def setNonnegative(b: Boolean): this.type = {
     this.nonnegative = b
     this
@@ -185,7 +188,7 @@ class PMF private (
    * set `spark.rdd.compress` to `true` to reduce the space requirement, at the cost of speed.
    */
   @DeveloperApi
-  @Since("1.1.0")
+//  @Since("1.1.0")
   def setIntermediateRDDStorageLevel(storageLevel: StorageLevel): this.type = {
     require(storageLevel != StorageLevel.NONE,
       "ALS is not designed to run without persisting intermediate RDDs.")
@@ -201,7 +204,7 @@ class PMF private (
    * at the cost of speed.
    */
   @DeveloperApi
-  @Since("1.3.0")
+//  @Since("1.3.0")
   def setFinalRDDStorageLevel(storageLevel: StorageLevel): this.type = {
     this.finalRDDStorageLevel = storageLevel
     this
@@ -215,7 +218,7 @@ class PMF private (
    * this setting is ignored.
    */
   @DeveloperApi
-  @Since("1.4.0")
+//  @Since("1.4.0")
   def setCheckpointInterval(checkpointInterval: Int): this.type = {
     this.checkpointInterval = checkpointInterval
     this
@@ -225,7 +228,7 @@ class PMF private (
    * Run ALS with the configured parameters on an input RDD of (user, product, rating) triples.
    * Returns a MatrixFactorizationModel with feature vectors for each user and product.
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def run(ratings: RDD[Rating]): MatrixFactorizationModel = {
     val sc = ratings.context
 
@@ -271,16 +274,16 @@ class PMF private (
   }
 
   /**
-   * Java-friendly version of [[ALS.run]].
+   * Java-friendly version of [[org.apache.spark.mllib.recommendation.ALS.run]].
    */
-  @Since("1.3.0")
+//  @Since("1.3.0")
   def run(ratings: JavaRDD[Rating]): MatrixFactorizationModel = run(ratings.rdd)
 }
 
 /**
  * Top-level methods for calling Alternating Least Squares (ALS) matrix factorization.
  */
-@Since("0.8.0")
+//@Since("0.8.0")
 object PMF {
   /**
    * Train a matrix factorization model given an RDD of ratings given by users to some products,
@@ -296,7 +299,7 @@ object PMF {
    * @param blocks     level of parallelism to split computation into
    * @param seed       random seed
    */
-  @Since("0.9.1")
+//  @Since("0.9.1")
   def train(
              ratings: RDD[Rating],
              rank: Int,
@@ -321,7 +324,7 @@ object PMF {
    * @param lambda     regularization factor (recommended: 0.01)
    * @param blocks     level of parallelism to split computation into
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def train(
              ratings: RDD[Rating],
              rank: Int,
@@ -344,7 +347,7 @@ object PMF {
    * @param iterations number of iterations of ALS (recommended: 10-20)
    * @param lambda     regularization factor (recommended: 0.01)
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def train(ratings: RDD[Rating], rank: Int, iterations: Int, lambda: Double)
   : MatrixFactorizationModel = {
     train(ratings, rank, iterations, lambda, -1)
@@ -361,7 +364,7 @@ object PMF {
    * @param rank       number of features to use
    * @param iterations number of iterations of ALS (recommended: 10-20)
    */
-  @Since("0.8.0")
+//  @Since("0.8.0")
   def train(ratings: RDD[Rating], rank: Int, iterations: Int)
   : MatrixFactorizationModel = {
     train(ratings, rank, iterations, 0.01, -1)
@@ -382,7 +385,7 @@ object PMF {
    * @param alpha      confidence parameter
    * @param seed       random seed
    */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def trainImplicit(
                      ratings: RDD[Rating],
                      rank: Int,
@@ -409,7 +412,7 @@ object PMF {
    * @param blocks     level of parallelism to split computation into
    * @param alpha      confidence parameter
    */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def trainImplicit(
                      ratings: RDD[Rating],
                      rank: Int,
@@ -434,7 +437,7 @@ object PMF {
    * @param lambda     regularization factor (recommended: 0.01)
    * @param alpha      confidence parameter
    */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def trainImplicit(ratings: RDD[Rating], rank: Int, iterations: Int, lambda: Double, alpha: Double)
   : MatrixFactorizationModel = {
     trainImplicit(ratings, rank, iterations, lambda, -1, alpha)
@@ -452,7 +455,7 @@ object PMF {
    * @param rank       number of features to use
    * @param iterations number of iterations of ALS (recommended: 10-20)
    */
-  @Since("0.8.1")
+//  @Since("0.8.1")
   def trainImplicit(ratings: RDD[Rating], rank: Int, iterations: Int)
   : MatrixFactorizationModel = {
     trainImplicit(ratings, rank, iterations, 0.01, -1, 1.0)
